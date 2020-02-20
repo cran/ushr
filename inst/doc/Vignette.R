cca00c21a7b8b6a84381f0895032aa1a76bedf67
Sinead E. Morris <sinead.morris@columbia.edu>
2020-02-20 16:50:02 +0000

    version 0.2.1

--- inst/doc/Vignette.R
+++ inst/doc/Vignette.R
@@ -1,38 +1,38 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------
 
 knitr::opts_chunk$set(echo = TRUE, fig.height = 6, fig.width = 8, message = FALSE, warning = FALSE)
 
 
-## ----load----------------------------------------------------------------
+## ----load---------------------------------------------------------------------
 
 library(ushr)
 
 print(head(actg315raw))
 
-## ----edit----------------------------------------------------------------
+## ----edit---------------------------------------------------------------------
 actg315 <- actg315raw %>%
     mutate(vl = 10^log10.RNA.) %>% 
     select(id = Patid, time = Day, vl)
 
 print(head(actg315))
 
-## ----plotdata, fig.height = 8, fig.width = 8-----------------------------
+## ----plotdata, fig.height = 8, fig.width = 8----------------------------------
 plot_data(actg315, detection_threshold = 100)
 
 
-## ----fits----------------------------------------------------------------
+## ----fits---------------------------------------------------------------------
 model_output <- ushr(data = actg315, detection_threshold = 100)
 
 
-## ----bpfits, fig.width = 6, fig.height = 4-------------------------------
+## ----bpfits, fig.width = 6, fig.height = 4------------------------------------
 plot_model(model_output, type = "biphasic", detection_threshold = 100)
 
 
-## ----spfits, fig.width = 3.5, fig.height = 2.5---------------------------
+## ----spfits, fig.width = 3.5, fig.height = 2.5--------------------------------
 plot_model(model_output, type = "single", detection_threshold = 100)
 
 
-## ----summariz------------------------------------------------------------
+## ----summariz-----------------------------------------------------------------
 actg315_summary <- summarize_model(model_output, data = actg315, stats = TRUE)
 
 head(actg315_summary$summary)
@@ -41,12 +41,12 @@ actg315_summary$biphasicstats
 
 actg315_summary$singlestats
 
-## ----CIs-----------------------------------------------------------------
+## ----CIs----------------------------------------------------------------------
 head(model_output$biphasicCI) 
 
 head(model_output$singleCI)     
 
-## ----TTSparametric-------------------------------------------------------
+## ----TTSparametric------------------------------------------------------------
 
 TTSparametric <- get_TTS(model_output = model_output, parametric = TRUE, 
                              suppression_threshold = 100)
@@ -54,7 +54,7 @@ head(TTSparametric)
 
 TTSparametric %>% summarize(median = median(TTS), SD = sd(TTS), N = n())
 
-## ----TTSnonparametric----------------------------------------------------
+## ----TTSnonparametric---------------------------------------------------------
 
 TTSnonparametric <- get_TTS(data = actg315, parametric = FALSE, 
                                 suppression_threshold = 100)
@@ -62,7 +62,7 @@ head(TTSnonparametric)
 
 TTSnonparametric %>% summarize(median = median(TTS), SD = sd(TTS), N = n())
 
-## ----TTSplot, fig.width = 2, fig.height = 2------------------------------
+## ----TTSplot, fig.width = 2, fig.height = 2-----------------------------------
 plot_TTS(TTSparametric, bins = 6, textsize = 7)
 plot_TTS(TTSnonparametric, bins = 6, textsize = 7)
 
